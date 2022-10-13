0017763e8795233e52ee2a7d49253932355b1c88
WhiteBox <artgroop.whitebox@gmail.com>
2022-10-14 01:52:50 +0300

    add cors(), static, add folders public, images, add file db, add get for foundUser, post for createdUser

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const db_1 = require("./db");
 const PORT = process.env.PORT || 7000;
 const app = (0, express_1.default)();
-app.get('/', (request, response) => {
-    console.log(`hi`);
-    response.json({ name: "Sveto4ka" });
+app.use((0, cors_1.default)());
+app.use('/static', express_1.default.static(__dirname + '/public'));
+const jsonBodyMiddlewear = express_1.default.json();
+app.use(jsonBodyMiddlewear);
+app.get('/users', (request, response) => {
+    console.log(db_1.db.users);
+    response.json(db_1.db.users);
 });
+app.get('/users/:id', (request, response) => {
+    const foundUser = db_1.db.users.find((el) => el.id === +request.params.id);
+    if (!foundUser) {
+        response.sendStatus(404);
+        return;
+    }
+    response.json(foundUser);
+});
+app.post('/users', (request, response) => {
+    const createdUser = {
+        id: +(new Date()),
+        nick: request.body.nick,
+        email: request.body.email,
+        password: request.body.password
+    };
+    db_1.db.users.push(createdUser);
+    response.json(createdUser);
+});
+// app.put('/users/:id', (request, response) => { 
+//     const createdAvatar = {
+//         avatar: request.body.avatar
+//     }
+//     db.users.push(createdAvatar)
+//     console.log(createdAvatar)
+//     response.json(createdAvatar)    
+// })
 app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}...`);
+    console.log(`CORS-enabled web server listening on port ${PORT}... This is CORS-enabled for all origins!`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
 import express from 'express'
-import path from 'path'
+import cors from 'cors'
+import { db } from './db'
 
 const PORT = process.env.PORT || 7000 
 const app = express()
 
-app.get('/', (request, response) => { 
-    console.log(`hi`)
-    response.json({name: "Sveto4ka"})    
+app.use(cors())
+app.use('/static', express.static(__dirname + '/public'));
+
+const jsonBodyMiddlewear = express.json()
+app.use(jsonBodyMiddlewear)
+
+app.get('/users', (request, response) => { 
+    console.log(db.users)
+    response.json(db.users)    
+})  
+
+app.get('/users/:id', (request, response) => { 
+    const foundUser = db.users.find((el: { id: number }) => el.id === +request.params.id)
+    if (!foundUser) {
+        response.sendStatus(404)
+        return
+    }
+    response.json(foundUser)    
+}) 
+
+app.post('/users', (request, response) => { 
+    const createdUser = {
+        id: +(new Date()),
+        nick: request.body.nick,
+        email: request.body.email,
+        password: request.body.password     
+    }
+    db.users.push(createdUser)       
+    response.json(createdUser) 
 })
 
+// app.put('/users/:id', (request, response) => { 
+//     const createdAvatar = {
+//         avatar: request.body.avatar
+//     }
+//     db.users.push(createdAvatar)
+//     console.log(createdAvatar)
+//     response.json(createdAvatar)    
+// })
+
 app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}...`)
+    console.log(`CORS-enabled web server listening on port ${PORT}... This is CORS-enabled for all origins!`)
 })
