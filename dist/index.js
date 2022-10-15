16d920ae389822bed1498babd8502eb6a7b49de9
WhiteBox <artgroop.whitebox@gmail.com>
2022-10-15 14:46:15 +0300

    add POST /upload for image

--- dist/index.js
+++ dist/index.js
@@ -4,43 +4,69 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const cors_1 = __importDefault(require("cors"));
 const db_1 = require("./db");
 const PORT = process.env.PORT || 7000;
 const app = (0, express_1.default)();
+const publicPath = process.env.PUBLIC_PATH || `C:\\Users\\ML\\APP artgroup_whitebox\\server_psy_diary\\public`;
+console.log("PATH P: ", publicPath + '/upload/');
 app.use((0, cors_1.default)());
-app.use('/static', express_1.default.static(__dirname + '/public'));
+app.use('/static', express_1.default.static(publicPath));
+// ограничить размер загружаемого файла
+app.use((0, express_fileupload_1.default)({
+    limits: { fileSize: 10000000 },
+    abortOnLimit: true
+}));
 const jsonBodyMiddlewear = express_1.default.json();
 app.use(jsonBodyMiddlewear);
-app.get('/users', (request, response) => {
+app.get('/users', (req, res) => {
     console.log(db_1.db.users);
-    response.json(db_1.db.users);
+    res.json(db_1.db.users);
 });
-app.get('/users/:id', (request, response) => {
-    const foundUser = db_1.db.users.find((el) => el.id === +request.params.id);
+app.get('/users/:id', (req, res) => {
+    const foundUser = db_1.db.users.find((el) => el.id === +req.params.id);
     if (!foundUser) {
-        response.sendStatus(404);
+        res.sendStatus(404);
         return;
     }
-    response.json(foundUser);
+    res.json(foundUser);
 });
-app.post('/users', (request, response) => {
+app.post('/users', (req, res) => {
     const createdUser = {
         id: +(new Date()),
-        nick: request.body.nick,
-        email: request.body.email,
-        password: request.body.password
+        nick: req.body.nick,
+        email: req.body.email,
+        password: req.body.password
     };
     db_1.db.users.push(createdUser);
-    response.json(createdUser);
+    res.json(createdUser);
 });
-// app.put('/users/:id', (request, response) => { 
+app.post('/upload', (req, res) => {
+    var _a;
+    // Log the files to the console
+    console.log(req.files);
+    // Get the file that was set to our field named "image"
+    // const { image } = req.files
+    const image = (_a = req.files) === null || _a === void 0 ? void 0 : _a.image;
+    // If no image submitted, exit
+    if (!image)
+        return res.status(400).send("Картинка не вставлена");
+    // If does not have image mime type prevent from uploading
+    if (!/^image/.test(image.mimetype))
+        return res.status(400).send("Это не картинка");
+    // Move the uploaded image to our upload folder    
+    image.mv(publicPath + '/upload/' + image.name);
+    // All good
+    res.status(200).json({ imageUrl: `http://localhost:${PORT}/static/upload/${image.name}` });
+});
+// app.put('/users/:id', (req, res) => { 
 //     const createdAvatar = {
-//         avatar: request.body.avatar
+//         avatar: req.body.avatar
 //     }
 //     db.users.push(createdAvatar)
 //     console.log(createdAvatar)
-//     response.json(createdAvatar)    
+//     res.json(createdAvatar)    
 // })
 app.listen(PORT, () => {
     console.log(`CORS-enabled web server listening on port ${PORT}... This is CORS-enabled for all origins!`);
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,80 @@
 import express from 'express'
+import fileUpload from 'express-fileupload'
 import cors from 'cors'
 import { db } from './db'
 
 const PORT = process.env.PORT || 7000 
 const app = express()
 
+const publicPath = process.env.PUBLIC_PATH || `C:\\Users\\ML\\APP artgroup_whitebox\\server_psy_diary\\public`
+console.log("PATH P: ", publicPath + '/upload/')
+
 app.use(cors())
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/static', express.static(publicPath))
+
+// ограничить размер загружаемого файла
+app.use(fileUpload({
+    limits: {fileSize: 10000000}, // Around 10MB        
+    abortOnLimit: true
+})
+)
 
 const jsonBodyMiddlewear = express.json()
 app.use(jsonBodyMiddlewear)
 
-app.get('/users', (request, response) => { 
+app.get('/users', (req, res) => { 
     console.log(db.users)
-    response.json(db.users)    
+    res.json(db.users)    
 })  
 
-app.get('/users/:id', (request, response) => { 
-    const foundUser = db.users.find((el: { id: number }) => el.id === +request.params.id)
+app.get('/users/:id', (req, res) => { 
+    const foundUser = db.users.find((el: { id: number }) => el.id === +req.params.id)
     if (!foundUser) {
-        response.sendStatus(404)
+        res.sendStatus(404)
         return
     }
-    response.json(foundUser)    
+    res.json(foundUser)    
 }) 
 
-app.post('/users', (request, response) => { 
+app.post('/users', (req, res) => { 
     const createdUser = {
         id: +(new Date()),
-        nick: request.body.nick,
-        email: request.body.email,
-        password: request.body.password     
+        nick: req.body.nick,
+        email: req.body.email,
+        password: req.body.password     
     }
     db.users.push(createdUser)       
-    response.json(createdUser) 
+    res.json(createdUser) 
 })
 
-// app.put('/users/:id', (request, response) => { 
+app.post('/upload', (req, res) => { 
+    // Log the files to the console
+    console.log(req.files)
+
+    // Get the file that was set to our field named "image"
+    // const { image } = req.files
+    const image = req.files?.image as fileUpload.UploadedFile
+
+    // If no image submitted, exit
+    if (!image) return res.status(400).send("Картинка не вставлена")
+
+    // If does not have image mime type prevent from uploading
+    if (!/^image/.test(image.mimetype)) return res.status(400).send("Это не картинка")
+
+    // Move the uploaded image to our upload folder    
+    image.mv(publicPath + '/upload/' + image.name)
+    
+    // All good
+    res.status(200).json({imageUrl: `http://localhost:${PORT}/static/upload/${image.name}`})
+})
+
+// app.put('/users/:id', (req, res) => { 
 //     const createdAvatar = {
-//         avatar: request.body.avatar
+//         avatar: req.body.avatar
 //     }
 //     db.users.push(createdAvatar)
 //     console.log(createdAvatar)
-//     response.json(createdAvatar)    
+//     res.json(createdAvatar)    
 // })
 
 app.listen(PORT, () => {
